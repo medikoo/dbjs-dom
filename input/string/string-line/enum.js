3070df7931e46c07fd9652e6ef06cd1ef204c614
Mariusz Nowak <mariusz@medikoo.com>
2013-01-19 21:32:33 +0100

    Radio control for Enum type

--- input/string/string-line/enum.js
+++ input/string/string-line/enum.js
@@ -5,6 +5,7 @@ var Db   = require('../../')
 
 module.exports = Enum;
 
+require('../../_controls/radio');
 require('../../_controls/select');
 
 Enum.set('chooseLabel',
@@ -26,3 +27,37 @@ Enum.set('DOMInputBox', Db.external(function () {
 	proto.constructor = Box;
 	return Box;
 }));
+
+Enum.set('DOMRadioBox', Db.external(function () {
+	var Parent, Box, proto;
+	Parent = this.Base.DOMRadioBox;
+	Box = function (document, ns, relation) {
+		Parent.call(this, document);
+		this.dom.classList.add('enum');
+		this.ns = ns;
+		this.relation = relation;
+		ns.options.forEach(function (value, item) {
+			this.dom.appendChild(this.createOption(value,
+				item._label.toDOM(document)));
+		}, this);
+	};
+	proto = Box.prototype = Object.create(Parent.prototype);
+	proto.constructor = Box;
+	return Box;
+}));
+
+Enum.set('toDOMInputBox', function (document/*, options*/) {
+	var box, options = Object(arguments[1]);
+	if (options.type === 'radio') {
+		box = new this.DOMRadioBox(document, this);
+	} else {
+		box = new this.DOMInputBox(document, this);
+	}
+	Object.keys(options).forEach(function (name) {
+		if ((name.indexOf('data-') === 0) ||
+				this.allowedDOMInputAttrs.has(name)) {
+			box.setAttribute(name, options[name]);
+		}
+	}, this);
+	return box;
+});
