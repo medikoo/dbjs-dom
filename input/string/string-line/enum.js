f31aca44e22b4a04e23b1e47ffb72a72d84a1b42
Mariusz Nowak <mariusz@medikoo.com>
2013-02-07 18:55:38 +0100

    Use live lists to display enum options

--- input/string/string-line/enum.js
+++ input/string/string-line/enum.js
@@ -1,11 +1,15 @@
 'use strict';
 
-var d           = require('es5-ext/lib/Object/descriptor')
-  , getObject   = require('dbjs/lib/objects')._get
-  , DOMRadio    = require('../../_controls/radio')
-  , DOMSelect   = require('../../_controls/select')
+var d              = require('es5-ext/lib/Object/descriptor')
+  , extend         = require('es5-ext/lib/Object/extend')
+  , replaceContent = require('dom-ext/lib/Element/prototype/replace-content')
+  , getObject      = require('dbjs/lib/objects')._get
+  , DOMRadio       = require('../../_controls/radio')
+  , DOMSelect      = require('../../_controls/select')
 
   , Enum = getObject('Enum'), StringLine = getObject('StringLine')
+  , createOption = DOMSelect.prototype.createOption
+  , createRadio = DOMRadio.prototype.createOption
 
   , Radio, Select;
 
@@ -15,26 +19,42 @@ Enum.set('chooseLabel', StringLine.rel({ required: true, value: 'Choose:' }));
 
 Select = function (document, ns) {
 	DOMSelect.call(this, document, ns);
-	this.dom.appendChild(this.createOption('',
-		ns._chooseLabel.toDOM(document)));
-	ns.options.forEach(function (value, item) {
-		this.dom.appendChild(this.createOption(value, item._label.toDOM(document)));
-	}, this);
+	this.chooseOption = createOption.call(this, '',
+		ns._chooseLabel.toDOM(document));
+	this.dbOptions = ns.options.itemsListByOrder()
+		.liveMap(this.createOption, this);
+	this.dbOptions.on('change', this.render);
+	this.render();
 };
-Select.prototype = Object.create(DOMSelect.prototype, {
-	constructor: d(Select)
-});
+Select.prototype = Object.create(DOMSelect.prototype, extend({
+	constructor: d(Select),
+	createOption: d(function (item) {
+		return createOption.call(this, item._subject_,
+			item._label.toDOM(this.document));
+	})
+}, d.binder({
+	render: d(function () {
+		replaceContent.call(this.dom, this.chooseOption, this.dbOptions);
+	})
+})));
 
 Radio = function (document, ns) {
 	DOMRadio.call(this, document, ns);
 	this.dom.classList.add('enum');
-	ns.options.forEach(function (value, item) {
-		this.dom.appendChild(this.createOption(value, item._label.toDOM(document)));
-	}, this);
+	this.dbOptions = ns.options.itemsListByOrder()
+		.liveMap(this.createOption, this);
+	this.dbOptions.on('change', this.render);
+	this.render();
 };
-Radio.prototype = Object.create(DOMRadio.prototype, {
-	constructor: d(Radio)
-});
+Radio.prototype = Object.create(DOMRadio.prototype, extend({
+	constructor: d(Radio),
+	createOption: d(function (item) {
+		return createRadio.call(this, item._subject_,
+			item._label.toDOM(this.document));
+	})
+}, d.binder({
+	render: d(function () { replaceContent.call(this.dom, this.dbOptions); })
+})));
 
 module.exports = Object.defineProperties(Enum, {
 	DOMRadio: d(Radio),
--- input/utils/fieldset.js
+++ input/utils/fieldset.js
@@ -1,56 +1,71 @@
 'use strict';
 
-var compact = require('es5-ext/lib/Array/prototype/compact')
-  , copy    = require('es5-ext/lib/Object/copy')
-  , d       = require('es5-ext/lib/Object/descriptor')
-  , extend  = require('es5-ext/lib/Object/extend')
-  , el      = require('dom-ext/lib/Document/prototype/make-element')
-  , Db      = require('../')
+var copy           = require('es5-ext/lib/Object/copy')
+  , d              = require('es5-ext/lib/Object/descriptor')
+  , extend         = require('es5-ext/lib/Object/extend')
+  , el             = require('dom-ext/lib/Document/prototype/make-element')
+  , replaceContent = require('dom-ext/lib/Element/prototype/replace-content')
+  , Db             = require('../')
 
   , map = Array.prototype.map
-  , Base = Db.Base, FunctionType = Db.Function
+  , Base = Db.Base
   , Fieldset;
 
 require('./fieldset-item');
 
 module.exports = Fieldset = function (document, obj/*, options*/) {
-	var options = Object(arguments[2]), names;
+	var options = Object(arguments[2]);
 
 	this.document = document;
 	this.obj = obj;
+	this.options = options;
 
-	if (options.names != null) names = options.names;
-	else names = obj.getPropertyNames(options.tag);
-
-	this.items = compact.call(map.call(names, function (name) {
-		var rel = obj['_' + name];
-		return ((rel.ns === Base) || (rel.ns === FunctionType)) ? null : rel;
-	})).sort(function (relA, relB) {
-		return relA.order - relB.order;
-	}).map(function (rel) {
-		var controlOpts;
-		if (options.control) controlOpts = copy(options.control);
-		if (options.controls && options.controls[rel.name]) {
-			controlOpts = extend(Object(controlOpts), options.controls[rel.name]);
+	if (options.names != null) {
+		this.names = options.names;
+	} else {
+		this.names = obj.getPropertyNames(options.tag);
+	}
+	if (this.names._isSet_) {
+		if (this.names.listByOrder) {
+			this.items = this.names.listByOrder().liveMap(this.renderItem, this);
+		} else {
+			this.items = this.names.values.map(this.renderItem, this);
 		}
-		if (options.idPostfix != null) {
-			controlOpts.id = rel.DOMId + options.idPostfix;
-		}
-		return rel.toDOMFieldsetItem(document, controlOpts);
-	});
+	} else {
+		this.items = map.call(this.names, this.renderItem, this);
+	}
 
 	this.build();
+	this.reload();
+
+	if (this.items._isLiveList_) this.items.on('change', this.reload);
+
 	this.dom.classList.add('dbjs');
 };
 
-Object.defineProperties(Fieldset.prototype, {
+Object.defineProperties(Fieldset.prototype, extend({
 	build: d(function () {
 		el = el.bind(this.document);
 		this.dom = el('fieldset', el('table',
-			this.domItems = el('tbody', this.items)));
+			this.domItems = el('tbody')));
+	}),
+	renderItem: d(function (name) {
+		var controlOpts, rel = this.obj['_' + name];
+		if (this.options.control) controlOpts = copy(this.options.control);
+		if (this.options.controls && this.options.controls[name]) {
+			controlOpts = extend(Object(controlOpts), this.options.controls[name]);
+		}
+		if (this.options.idPostfix != null) {
+			controlOpts.id = rel.DOMId + this.options.idPostfix;
+		}
+		return rel.toDOMFieldsetItem(this.document, controlOpts);
 	}),
 	toDOM: d(function () { return this.dom; })
-});
+}, d.binder({
+	reload: d(function () {
+		replaceContent.call(this.domItems, this.items);
+	})
+})));
 
 Object.defineProperty(Base, 'DOMFieldset', d(Fieldset));
 
