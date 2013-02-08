1bbd54e1fbedeb337fd33610ead89d81e6ecf2c9
Mariusz Nowak <mariusz@medikoo.com>
2013-02-08 14:52:54 +0100

    Use labels definied on relation if available

--- text/boolean.js
+++ text/boolean.js
@@ -8,9 +8,10 @@ var d        = require('es5-ext/lib/Object/descriptor')
   , Base = Db.Base
   , Text;
 
-Text = function (document, ns) {
+Text = function (document, ns, options) {
 	this.document = document;
 	this.ns = ns;
+	this.relation = options && options.relation;
 	this.text = new DOMText(document, Base);
 	this.dom = this.text.dom;
 };
@@ -18,13 +19,20 @@ Text = function (document, ns) {
 Text.prototype = Object.create(DOMText.prototype, {
 	constructor: d(Text),
 	value: d.gs(getValue, function (value) {
+		var rel;
 		if (value == null) {
 			this.text.dismiss();
 			this.text.value = value;
 			return;
 		}
-		this.ns[value.valueOf() ? '_trueLabel' : '_falseLabel']
-			.assignDOMText(this.text);
+		if (value.valueOf()) {
+			rel = (this.relation && this.relation.__trueLabel.__value) ?
+					this.relation._trueLabel : this.ns._trueLabel;
+		} else {
+			rel = (this.relation && this.relation.__falseLabel.__value) ?
+					this.relation._falseLabel : this.ns._falseLabel;
+		}
+		rel.assignDOMText(this.text);
 	})
 });
 
