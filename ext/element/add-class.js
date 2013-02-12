3003df2e759113fc2f0074663aa970389a2fb9fb
Mariusz Nowak <mariusz@medikoo.com>
2013-02-12 13:01:35 +0100

    HTML Class handlers

---
+++ ext/element/add-class.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var element = require('dom-ext/lib/Element/valid-element')
+
+  , prefix = '_dbjs:relation:class.';
+
+module.exports = function (value) {
+	var current;
+	element(this);
+	if (!value || (value._type_ !== 'relation')) {
+		this.classList.add(value);
+		return this;
+	}
+	if (this.hasOwnProperty(prefix + value._id_)) return;
+	current = value.objectValue;
+	if (current != null) this.classList.add(current);
+	value.on('change', this[prefix + value._id_] = function (nu, old) {
+		if (current != null) this.classList.remove(current);
+		current = value.objectValue;
+		if (current != null) this.classList.add(current);
+	}.bind(this));
+};
---
+++ ext/element/remove-class.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var element = require('dom-ext/lib/Element/valid-element')
+
+  , prefix = '_dbjs:relation:class.';
+
+module.exports = function (value) {
+	element(this);
+	if (!value || (value._type_ !== 'relation')) {
+		this.classList.remove(value);
+		return this;
+	}
+	if (!this.hasOwnProperty(prefix + value._id_)) return;
+	value.off('change', this[prefix + value._id_]);
+	delete this[prefix + value._id_];
+	value = value.objectValue;
+	if (value != null) this.classList.remove(value);
+};
