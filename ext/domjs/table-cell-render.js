f7b1b082f60f187bb742bd59f8e96eb478e3d820
Mariusz Nowak <mariusz@medikoo.com>
2013-02-13 15:32:05 +0100

    domjs tableCellRenderer extension

---
+++ ext/domjs/table-cell-render.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var d     = require('es5-ext/lib/Object/descriptor')
+  , Table = require('../../text/utils/table')
+
+  , cellRender = Table.prototype.cellRender;
+
+var customRender = function (render, item) {
+	var direct, df;
+	df = this.domjs.collect(function () { direct = render(item); });
+	return direct || df;
+};
+
+Object.defineProperty(Table.prototype, 'cellRender', d(function (render, item) {
+	if (!this.domjs) return cellRender.call(this, render, item);
+	return cellRender.call(this, customRender.bind(this, render), item);
+}));
