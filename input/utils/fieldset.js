e1bbb8540faa20e8096fd00b7eebf56e918459f9
Mariusz Nowak <mariusz@medikoo.com>
2013-03-04 15:14:02 +0100

    Base DOMFieldset on relations set.
    Remove names related configuration to `toDOMFieldset`.
    Additionally support `include` option

--- input/utils/fieldset.js
+++ input/utils/fieldset.js
@@ -1,45 +1,39 @@
 'use strict';
 
-var copy           = require('es5-ext/lib/Object/copy')
+var CustomError    = require('es5-ext/lib/Error/custom')
+  , copy           = require('es5-ext/lib/Object/copy')
   , d              = require('es5-ext/lib/Object/descriptor')
   , extend         = require('es5-ext/lib/Object/extend')
+  , forEach        = require('es5-ext/lib/Object/for-each')
+  , contains       = require('es5-ext/lib/String/prototype/contains')
   , makeElement    = require('dom-ext/lib/Document/prototype/make-element')
   , replaceContent = require('dom-ext/lib/Element/prototype/replace-content')
+  , SetCollection  = require('set-collection')
   , Db             = require('../')
 
   , map = Array.prototype.map
+  , getRel = function (name) { return this.get(name); }
   , Base = Db.Base
   , Fieldset;
 
 require('./fieldset-item');
 
-module.exports = Fieldset = function (document, obj/*, options*/) {
+module.exports = Fieldset = function (document, list/*, options*/) {
 	var options = Object(arguments[2]), className;
 
 	this.document = document;
-	this.obj = obj;
+	this.list = list;
 	this.options = options;
 
-	if (options.names != null) {
-		this.names = options.names;
+	if (list._isLiveList) {
+		this.items = list.liveMap(this.renderItem, this);
+		this.items.on('change', this.reload);
 	} else {
-		this.names = obj.getPropertyNames(options.tag);
-		if (options.filter) this.names = this.names.filter(options.filter);
-	}
-	if (this.names._isSet_) {
-		if (this.names.listByOrder) {
-			this.items = this.names.listByOrder().liveMap(this.renderItem, this);
-		} else {
-			this.items = this.names.values.map(this.renderItem, this);
-		}
-	} else {
-		this.items = map.call(this.names, this.renderItem, this);
+		this.items = list.map(this.renderItem, this);
 	}
 
 	this.render();
 	this.reload();
-
-	if (this.items._isLiveList_) this.items.on('change', this.reload);
 
 	this.dom.classList.add('dbjs');
 	if (options.class != null) {
@@ -58,11 +52,12 @@ Object.defineProperties(Fieldset.prototype, extend({
 		this.dom = el('fieldset', el('table',
 			this.domItems = el('tbody')));
 	}),
-	renderItem: d(function (name) {
-		var controlOpts, rel = this.obj['_' + name];
+	renderItem: d(function (rel) {
+		var controlOpts;
 		if (this.options.control) controlOpts = copy(this.options.control);
-		if (this.options.controls && this.options.controls[name]) {
-			controlOpts = extend(Object(controlOpts), this.options.controls[name]);
+		if (this.options.controls && this.options.controls[rel._id_]) {
+			controlOpts = extend(Object(controlOpts),
+				this.options.controls[rel._id_]);
 		}
 		if (this.options.idPostfix != null) {
 			controlOpts.id = rel.DOMId + this.options.idPostfix;
@@ -81,5 +76,69 @@ Object.defineProperty(Base, 'DOMFieldset', d(Fieldset));
 
 Object.defineProperty(Db.prototype, 'toDOMFieldset',
 	d(function (document/*, options*/) {
-		return new Base.DOMFieldset(document, this, arguments[1]);
+		var options = Object(arguments[1]), data, list, include, controlOpts
+		  , setup, byOrder;
+
+		if (options.names != null) {
+			data = options.names._isSet_ ? options.names.values : options.names;
+			data = new SetCollection(map.call(data, getRel, this));
+		} else {
+			data = this.getProperties(options.tag);
+		}
+
+		if (options.include) {
+			if (options.include._type_ === 'relation') {
+				include = new SetCollection();
+				include.add(options.include);
+			} else if (!options.include._isSet_) {
+				include = new SetCollection(options.include);
+			} else {
+				include = options.include;
+			}
+			include.forEach(function (rel) {
+				if (!rel || (rel._type_ !== 'relation')) {
+					throw new CustomError("Include item must be a relation",
+						'INVALID_RELATION');
+				}
+			});
+			data = data.union(include);
+		}
+
+		if (options.controls) {
+			forEach(options.controls, function (value, name, options) {
+				if (contains.call(name, ':')) return;
+				options[this._id_ + ':' + name] = value;
+				delete options[name];
+			}, this);
+		}
+		controlOpts = Object(options.controls);
+
+		byOrder = function (a, b) {
+			var aOpts = controlOpts[a._id_], bOpts = controlOpts[b._id_];
+			a = (aOpts && !isNaN(aOpts.order)) ? aOpts.order : a.order;
+			b = (bOpts && !isNaN(bOpts.order)) ? bOpts.order : b.order;
+			return a - b;
+		};
+
+		if (data.list) {
+			list = data.list(byOrder);
+			list.forEach(function setup(rel) {
+				var opts = controlOpts[rel._id_];
+				if (opts && !isNaN(opts.order)) return;
+				rel._order.on('change', list._sort);
+			});
+			data.on('add', function (rel) {
+				setup(rel);
+				list._sort();
+			});
+			data.on('delete', function (rel) {
+				var opts = controlOpts[rel._id_];
+				if (opts && !isNaN(opts.order)) return;
+				rel._order.off('change', list._sort);
+			});
+		} else {
+			list = data.values.sort(byOrder);
+		}
+
+		return new Base.DOMFieldset(document, list, options);
 	}));
