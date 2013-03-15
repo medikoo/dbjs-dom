3754efcc58d114117eef9206fa93fc9e10a794b5
Mariusz Nowak <mariusz@medikoo.com>
2013-03-15 13:57:58 +0100

    Base EnumOther on InputComponent

---
+++ input/components/_get-fields.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var CustomError = require('es5-ext/lib/Error/custom')
+  , forEach     = require('es5-ext/lib/Object/for-each')
+  , getObject   = require('dbjs/lib/objects')._get
+
+  , getName, getRel;
+
+getName = function (rel, name, ns) {
+	var field = rel['__' + name];
+	if (field) field = field.__value;
+
+	if (field == null) {
+		throw new CustomError("Missing field name setting", 'NO_FIELD_SETTING');
+	}
+	return field;
+};
+
+getRel = function (obj, name, ns) {
+	var rel = obj.get(name);
+	if ((rel.ns !== ns) && !ns.isPrototypeOf(rel.ns)) {
+		throw new CustomError("Namespace mismatch", 'WRONG_NAMESPACE');
+	}
+	return rel;
+};
+
+module.exports = function (rel, data) {
+	var result = { names: {}, rels: {} };
+	forEach(data, function (ns, name) {
+		var propName;
+		ns = getObject(ns);
+		result.names[name] = propName = getName(rel, name, ns);
+		result.rels[propName] = getRel(rel.obj, propName, ns);
+	});
+	return result;
+};
--- input/components/enum-other.js
+++ input/components/enum-other.js
@@ -3,49 +3,37 @@
 var CustomError = require('es5-ext/lib/Error/custom')
   , copy        = require('es5-ext/lib/Object/copy')
   , d           = require('es5-ext/lib/Object/descriptor')
-  , getObject   = require('dbjs/lib/objects')._get
+  , Input       = require('../_component')
+  , getFields   = require('./_get-fields')
 
   , defineProperty = Object.defineProperty
-  , getField, getRel, toDOMInput;
-
-getField = function (rel, name, ns) {
-	var field = rel['__' + name];
-	if (field) field = field.__value;
-
-	if (field == null) {
-		throw new CustomError("Missing field name setting", 'NO_FIELD_SETTING');
-	}
-	return getRel(rel.obj, field, ns);
-};
-
-getRel = function (obj, name, ns) {
-	var rel = obj.get(name);
-	if ((rel.ns !== ns) && !ns.isPrototypeOf(rel.ns)) {
-		throw new CustomError("Namespace mismatch", 'WRONG_NAMESPACE');
-	}
-	return rel;
-};
+  , toDOMInput;
 
 toDOMInput = function (document/*, options*/) {
-	var enumField = getField(this, 'enumField', getObject('Enum'))
-	  , otherField = getField(this, 'otherField', getObject('StringLine'))
-	  , options = Object(arguments[1]), dom, enumOpts, otherItem, otherInput;
+	var data = getFields(this, { enumField: 'Enum', otherField: 'StringLine' })
+	  , options = Object(arguments[1]), inputs = {}, controlOpts
+	  , enumInput, other, otherItem, dom;
 
-	enumOpts = copy(options);
-	enumOpts.type = 'radio';
-	dom = enumField.toDOMInput(document, enumOpts);
-	otherItem = dom.listItems.other.firstChild;
+	controlOpts = copy(options);
+	controlOpts.type = 'radio';
+	inputs[data.names.enumField] = enumInput =
+		data.rels[data.names.enumField].toDOMInput(document, controlOpts);
+	otherItem = enumInput.listItems.other.firstChild;
 	if (!otherItem) {
 		throw new CustomError("Other item not found", 'OTHER_NOT_FOUND');
 	}
 	otherItem.appendChild(document.createTextNode(': '));
-	otherItem.appendChild(otherInput =
-		otherField.toDOMInput(document, options).toDOM());
+	inputs[data.names.otherField] = other =
+		data.rels[data.names.otherField].toDOMInput(document, options);
+	otherItem.appendChild(other.dom);
 
-	dom.on('change', function () {
-		otherInput.disabled = (dom.value !== 'other');
+	enumInput.on('change', function () {
+		other.control.disabled = (enumInput.value !== 'other');
 	});
-	return dom;
+
+	dom = document.createElement('div');
+	dom.appendChild(enumInput.dom);
+	return new Input(this, inputs, data.rels, dom);
 };
 
 module.exports = function (rel) {
