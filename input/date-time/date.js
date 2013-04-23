ba5f3a345d0faadb4303973d8f1f49611c6b8b4c
Mariusz Nowak <mariusz@medikoo.com>
2013-04-23 18:46:08 +0200

    Fix time zone shift issue, related to Date fields

--- input/date-time/date.js
+++ input/date-time/date.js
@@ -4,6 +4,8 @@ var d        = require('es5-ext/lib/Object/descriptor')
   , DOMInput = require('../date-time').DOMInput
 
   , DateType = require('dbjs/lib/objects')._get('Date')
+  , setValue = Object.getOwnPropertyDescriptor(DOMInput.prototype, 'value').set
+  , re = /^(\d{4})-(\d{2})-(\d{2})/
   , Input;
 
 require('../');
@@ -21,7 +23,19 @@ Input.prototype = Object.create(DOMInput.prototype, {
 	constructor: d(Input),
 	_dateToInputValue: d(function (date) {
 		return date.toISOString().slice(0, 10);
-	})
+	}),
+	value: d.gs(function () {
+		var value = this.dom.value, match;
+		if (!value) return null;
+		match = String(value).match(re);
+		if (!match) return null;
+		value = this.ns.normalize(new Date(Number(match[1]), Number(match[2]) - 1,
+			Number(match[3]), 12));
+		if (this._value && (value.valueOf() === this._value.valueOf())) {
+			return this._value;
+		}
+		return value;
+	}, setValue)
 });
 
 module.exports = Object.defineProperties(DateType, {
