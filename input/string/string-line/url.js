aa097ebb84b3201e75240f06b7a265c23e06fe4b
Mariusz Nowak <mariusz@medikoo.com>
2013-05-27 23:24:43 +0200

    Url input

---
+++ input/string/string-line/url.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var copy     = require('es5-ext/lib/Object/copy')
+  , d        = require('es5-ext/lib/Object/descriptor')
+  , extend   = require('es5-ext/lib/Object/extend')
+  , DOMInput = require('../string-line').DOMInput
+
+  , Url = require('dbjs/lib/objects')._get('Url')
+  , Input;
+
+require('../../');
+
+Input = function (document, ns/*, options*/) {
+	DOMInput.apply(this, arguments);
+};
+
+Input.prototype = Object.create(DOMInput.prototype, {
+	constructor: d(Input),
+	controlAttributes: d(extend(copy(DOMInput.prototype.controlAttributes),
+		{ dirname: false, inputmode: false })),
+	dbAttributes: d(extend(copy(DOMInput.prototype.dbAttributes),
+		{ pattern: false })),
+	_render: d(function () {
+		var input = this.control = this.dom = this.document.createElement('input');
+		input.setAttribute('type', 'url');
+	}),
+});
+
+module.exports = Object.defineProperties(Url, {
+	DOMInput: d(Input)
+});
