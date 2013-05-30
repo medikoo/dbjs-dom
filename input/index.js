09c0d1759111ccef5aa592461319fc12ef70869a
Mariusz Nowak <mariusz@medikoo.com>
2013-05-30 18:28:53 +0200

    Add dateTime control to index

--- input/index.js
+++ input/index.js
@@ -5,6 +5,7 @@ module.exports = require('../text');
 require('./_relation');
 require('./base');
 require('./boolean');
+require('./date-time');
 require('./number');
 require('./object');
 require('./string');
