00e95f8675667977500e9754dfc7a737ed57b26e
Mariusz Nowak <mariusz@medikoo.com>
2014-01-27 22:01:00 +0100

    Time input

---
+++ input/number/integer/u-integer/time.js
@@ -0,0 +1,57 @@
+'use strict';
+
+var pad      = require('es5-ext/number/#/pad')
+  , memoize  = require('memoizee/lib/regular')
+  , d        = require('d/d')
+  , setup    = require('../../../')
+  , DOMInput = require('../../../5.date-time').Input
+  , PreInput = require('../../../_controls/input')
+
+  , defineProperties = Object.defineProperties
+  , floor = Math.floor, min = Math.min, round = Math.round
+  , re = /^(\d{2}):(\d{2})$/
+  , castControlAttribute = PreInput.prototype.castControlAttribute
+  , Input;
+
+Input = function (document, type/*, options*/) {
+	DOMInput.apply(this, arguments);
+};
+
+Input.prototype = Object.create(DOMInput.prototype, {
+	constructor: d(Input),
+	_render: d(function () {
+		var input = this.control = this.dom = this.document.createElement('input');
+		input.setAttribute('type', 'time');
+	}),
+	castControlAttribute: d(function (name, value) {
+		if (this.dateAttributes[name] && !isNaN(value)) {
+			value = this.type.toInputValue(value);
+		}
+		if (name === 'step') value = min(round(value / 1000), 1);
+		castControlAttribute.call(this, name, value);
+	})
+});
+
+module.exports = exports = memoize(function (db) {
+	defineProperties(setup(db).Time, {
+		fromInputValue: d(function (value) {
+			var match;
+			if (value == null) return null;
+			value = value.trim();
+			if (!value) return null;
+			match = value.match(re);
+			if (!match) return null;
+			return this.normalize((Number(match[1]) * 1000 * 60 * 60) +
+				(Number(match[2]) * 1000 * 60));
+		}),
+		toInputValue: d(function (value) {
+			var minutes;
+			if (value == null) return null;
+			minutes = floor(value / (1000 * 60));
+			return pad.call(floor(minutes / 60), 2) + ':' + pad.call(minutes % 60, 2);
+		}),
+		DOMInput: d(Input)
+	});
+});
+
+exports.Input = Input;
