f48b71d3c2e3411161bc92750f27eb61b6c7dbef
Mariusz Nowak <mariusz@medikoo.com>
2014-09-01 15:16:52 +0200

    Fix names in data set

--- input/composites/enum-other.js
+++ input/composites/enum-other.js
@@ -17,27 +17,22 @@ Input.prototype = Object.create(DOMInput.prototype, {
 		  , data = getFields(this.observable.object, desc,
 				{ enumField: desc.database.StringLine,
 					otherField: desc.database.StringLine })
-		  , inputOpts, enumInput, other, otherItem;
+		  , inputOpts, enumInput, other, otherItem, observable;
 
-		inputOpts = this.getOptions(data.observables[data.names.enumField]);
+		observable = data.observables[data.names.enumField];
+		inputOpts = this.getOptions(observable);
 		inputOpts.type = 'radio';
-		this.addItem(enumInput =
-			data.observables[data.names.enumField]
-			.toDOMInput(this.document, inputOpts), 'enum');
+		this.addItem(enumInput = observable.toDOMInput(this.document, inputOpts), observable.dbId);
 		otherItem = enumInput.listItems.other.firstChild;
-		if (!otherItem) {
-			throw customError("Other item not found", 'OTHER_NOT_FOUND');
-		}
+		if (!otherItem) throw customError("Other item not found", 'OTHER_NOT_FOUND');
 		otherItem.appendChild(this.document.createTextNode(': '));
 
-		this.addItem(other =
-			data.observables[data.names.otherField].toDOMInput(this.document,
-				this.getOptions(data.observables[data.names.otherField])), 'other');
+		observable = data.observables[data.names.otherField];
+		this.addItem(other = observable.toDOMInput(this.document, this.getOptions(observable)),
+			observable.dbId);
 		otherItem.appendChild(other.dom);
 
-		enumInput.on('change', function () {
-			other.control.disabled = (enumInput.value !== 'other');
-		});
+		enumInput.on('change', function () { other.control.disabled = (enumInput.value !== 'other'); });
 
 		this.dom = this.document.createElement('div');
 		this.dom.appendChild(enumInput.dom);
