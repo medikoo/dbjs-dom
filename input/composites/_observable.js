2ae2e456c3cdfce40640e8b6827b43c733e09393
Mariusz Nowak <mariusz@medikoo.com>
2014-09-01 11:44:54 +0200

    Fix resolution of value in composites

--- input/composites/_observable.js
+++ input/composites/_observable.js
@@ -1,12 +1,16 @@
 'use strict';
 
-var noop     = require('es5-ext/function/noop')
+var last     = require('es5-ext/array/#/last')
+  , noop     = require('es5-ext/function/noop')
+  , mapKeys  = require('es5-ext/object/map-keys')
   , callable = require('es5-ext/object/valid-callable')
   , d        = require('d')
+  , splitId  = require('dbjs/_setup/unserialize/id')
   , DOMInput = require('../_composite')
 
   , getPrototypeOf = Object.getPrototypeOf
   , getInputValue = Object.getOwnPropertyDescriptor(DOMInput.prototype, 'inputValue').get
+  , mapKey = function (id) { return last.call(splitId(id)); }
   , Input;
 
 module.exports = Input = function (document, type/*, options*/) {
@@ -28,7 +32,7 @@ Input.prototype = Object.create(DOMInput.prototype, {
 	constructor: d(Input),
 	name: d.gs(noop, noop),
 	inputValue: d.gs(function () {
-		var state = getInputValue.call(this);
+		var state = mapKeys(getInputValue.call(this), mapKey);
 		return this.getValue.call(state);
 	}, noop)
 });
