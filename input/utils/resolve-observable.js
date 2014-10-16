ff3336448f09009550d5755221def1d9870f1a78
Mariusz Nowak <medyk@medikoo.com>
2014-10-16 09:43:13 +0200

    Update up to changes in dbjs

--- input/utils/resolve-observable.js
+++ input/utils/resolve-observable.js
@@ -1,21 +1,8 @@
 'use strict';
 
-var value         = require('es5-ext/object/valid-value')
-  , unserializeId = require('dbjs/_setup/unserialize/id')
-  , object        = require('dbjs/valid-dbjs-object')
-  , nested        = require('dbjs/valid-dbjs-nested-object');
+var resolve = require('dbjs/_setup/utils/resolve-property-path');
 
 module.exports = function (obj, id) {
-	var names = [];
-	object(obj);
-	names = [];
-	unserializeId(value(id)).forEach(function (token, index) {
-		if (index % 2) {
-			if (token !== '/') throw new TypeError(id + " is not property id");
-			return;
-		}
-		names.push(token);
-	});
-	while (names.length > 1) obj = nested(obj[names.shift()]);
-	return obj._get(names[0]);
+	var result = resolve(obj, id);
+	return result.object.getObservable(result.key);
 };
