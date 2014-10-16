40bf977b0d8feb27d36aca970efaadced8af8d43
Mariusz Nowak <medyk@medikoo.com>
2014-10-16 12:44:30 +0200

    Properly resolve static properties

--- input/utils/resolve-observable.js
+++ input/utils/resolve-observable.js
@@ -4,5 +4,6 @@ var resolve = require('dbjs/_setup/utils/resolve-property-path');
 
 module.exports = function (obj, id) {
 	var result = resolve(obj, id);
+	if (result.object.isKeyStatic(result.key)) return result.object[result.key];
 	return result.object.getObservable(result.key);
 };
