d1cca6fab5050968b92f28f703198dd3ae7471eb
Mariusz Nowak <medyk@medikoo.com>
2014-11-13 12:44:30 +0100

    Assure we return copy of an object
    There are rare cases when we want to alter options object,
    and resolveOptions gives an assumptions we always receive a copy
    when it was not always a case

--- input/utils/resolve-options.js
+++ input/utils/resolve-options.js
@@ -1,11 +1,12 @@
 'use strict';
 
-var normalizeOptions = require('es5-ext/object/normalize-options')
+var copy             = require('es5-ext/object/copy')
+  , normalizeOptions = require('es5-ext/object/normalize-options')
 
   , resolvedMark = { _optionsResolved: true };
 
 module.exports = function (options, type) {
-	if (options && options._optionsResolved) return options;
+	if (options && options._optionsResolved) return copy(options);
 	return normalizeOptions(type.inputOptions,
 		options && options.dbOptions && options.dbOptions.inputOptions, options, resolvedMark);
 };
