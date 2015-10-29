738c3cdae7ca1a1d6a5e06d480d964b1f8807029
Marek Tuchowski <marek@tuchowski.com.pl>
2015-10-29 16:15:08 +0100

    Issue #3. Added support for optionalInfo tooltip icon for inputs.

--- input/utils/get-component-render.js
+++ input/utils/get-component-render.js
@@ -5,21 +5,31 @@ var makeElement = require('dom-ext/document/#/make-element')
   , i18nToDom   = require('i18n2-md-to-dom')
   , memoize     = require('memoizee/weak-plain')
 
-  , mdiOptions = { inline: true };
+  , inlineMdiOption = { inline: true };
 
-var getMdi = memoize(function (document) {
+var getMd = memoize(function (document, mdiOptions) {
 	var mdi = i18nToDom(document);
 	return function (message) { return mdi(message, mdiOptions); };
 });
 
 module.exports = function (containerName) {
 	return function (input, options) {
-		var el = makeElement.bind(input.document), mdi = getMdi(input.document);
+		var el = makeElement.bind(input.document)
+		  , mdi = getMd(input.document, inlineMdiOption)
+		  , md = getMd(input.document);
+
 		return el(containerName, { class: options.class },
 			(options.label && [el('label',
 				(input.control ? { for: getId.call(input.control) } : null),
 				options.label, ' ')]) || null,
 			el('div', { class: 'input' }, input,
+				// info tooltip
+				options.optionalInfo && el('div', { class: 'input-optional-info' },
+					el('span', { class: 'fa fa-info-circle' }, "Info"),
+					el('div', { class: 'input-optional-info-content' },
+						typeof options.optionalInfo === 'string' ? md(options.optionalInfo)
+							: options.optionalInfo)),
+				// statuses
 				options.disabled ? null : el('span', { class: 'statuses' },
 					// missing (required field) mark
 					el('span', { class: 'status-missing' }, '★'),
